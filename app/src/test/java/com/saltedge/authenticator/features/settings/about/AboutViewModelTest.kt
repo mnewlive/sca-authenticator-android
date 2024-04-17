a4d77c401f66508fba00d9f9d26dde5086c9f30e
mnewlive <morozovvadim91@gmail.com>
2024-04-17 12:11:21 +0300

    Raised version

--- app/src/test/java/com/saltedge/authenticator/features/settings/about/AboutViewModelTest.kt
+++ app/src/test/java/com/saltedge/authenticator/features/settings/about/AboutViewModelTest.kt
@@ -74,7 +74,7 @@ class AboutViewModelTest : ViewModelTest() {
             equalTo(listOf(
                 SettingsItemViewModel(
                     titleId = R.string.about_app_version,
-                    description = "3.7.0"
+                    description = "3.7.1"
                 ),
                 SettingsItemViewModel(
                     titleId = R.string.about_copyright,
