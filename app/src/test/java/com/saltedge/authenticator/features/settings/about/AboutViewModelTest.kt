50b37c28a2a12221881632c86ad03cf156fcdde7
mnewlive <morozovvadim91@gmail.com>
2025-08-01 15:54:17 +0300

    Fix UI, raised version

--- app/src/test/java/com/saltedge/authenticator/features/settings/about/AboutViewModelTest.kt
+++ app/src/test/java/com/saltedge/authenticator/features/settings/about/AboutViewModelTest.kt
@@ -74,7 +74,7 @@ class AboutViewModelTest : ViewModelTest() {
             equalTo(listOf(
                 SettingsItemViewModel(
                     titleId = R.string.about_app_version,
-                    description = "3.7.3"
+                    description = "3.8.0"
                 ),
                 SettingsItemViewModel(
                     titleId = R.string.about_copyright,
